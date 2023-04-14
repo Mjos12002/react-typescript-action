d5005e158b075efdd6b3aae128200fb16300337e
Joseph MANZI <mjos12002@gmail.com>
2023-04-14 14:23:22 +0200

    Added middle principal and footer section

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -2,11 +2,25 @@ import React from "react";
 
 function User(){
     return (
-        <div>
-            <ul>
-                <li>Sign In</li>
-                <li>Sign Up</li>
-            </ul>
+        <div className="main-parent">
+            <div className="header-section">
+                <ul>
+                    <li>Home</li>
+                    <li>Register</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+            <div className="principal-section">
+                <div className="left">
+                    Left
+                </div>
+                <div className="right">
+                    Right
+                </div>
+                <div className="footer">
+                    Footer
+                </div>
+            </div>
         </div>
     )
 }
