ecced6c20cf294d85bec2765984fb23733482471
Joseph MANZI <mjos12002@gmail.com>
2023-04-14 14:19:10 +0200

    Added User function

---
+++ src/component/User.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function User(){
+    return (
+        <div>
+            <ul>
+                <li>Sign In</li>
+                <li>Sign Up</li>
+            </ul>
+        </div>
+    )
+}
+
+export default User;
